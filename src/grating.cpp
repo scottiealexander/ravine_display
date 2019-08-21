ad2b407b0729d55020f05e787950fa3fa186d4c7
scottiealexander <scottiealexander11@gmail.com>
2019-08-21 14:37:13 -0700

    Update main routine to use RunSpec and new run function

--- src/grating.cpp
+++ src/grating.cpp
@@ -1,7 +1,7 @@
 // Include standard headers
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+// #include <unistd.h>
 
 // Include GLEW
 #include <GL/glew.h>
@@ -10,33 +10,56 @@
 #include <GLFW/glfw3.h>
 
 // Include GLM
-#include <glm/glm.hpp>
-using namespace glm;
-
+// #include <glm/glm.hpp>
+// using namespace glm;
 
 #include <util/initialize.hpp>
 #include <util/program.hpp>
 #include <util/parse_ini.hpp>
+#include <util/run.hpp>
 
-#include "utility_classes.hpp"
+#include "./utility_classes.hpp"
 
-/* 2 * PI */
-#define TWOPI 6.2831855f
-
-/* Conversion factor from degrees to radians: */
-#define DEG2RAD 0.017453292f
-
-int get_refresh_rate()
+void usage()
 {
-    GLFWmonitor* primary = glfwGetPrimaryMonitor();
-    const GLFWvidmode* mode = glfwGetVideoMode(primary);
-    return mode->refreshRate;
+    printf("Usage: grating_ex <options> <ini_filepath>\n");
+    printf("Options:\n");
+    printf("    -f: fullscreen\n");
 }
 
 int main(int narg, char** args)
 {
+
+    char* inipath;
+    bool fullscreen = false;
+    if (narg < 2)
+    {
+        usage();
+        return -1;
+    }
+    else if (narg == 2)
+    {
+        inipath = args[1];
+    }
+    else if (narg > 2)
+    {
+        std::string opt(args[1]);
+        if (opt == "-f") { fullscreen = true; }
+        inipath = args[narg-1];
+    }
+
+    RunSpec rs(inipath);
+
+    if (!rs.isvalid())
+    {
+        printf("[ERROR]: failed to open file \"%s\"\n", inipath);
+        return -2;
+    }
+
+    rs.show();
+
     int ret;
-    GLFWwindow* window = init(false, ret);
+    GLFWwindow* window = init(fullscreen, ret);
 
     if ((ret < 0) || (window == NULL))
     {
@@ -45,39 +68,6 @@ int main(int narg, char** args)
     }
 
     glfwSwapInterval(1);
-    float refresh_interval = 1.0f / (float)get_refresh_rate();
-
-    float g_orientation = 0.0f;
-    float g_temporal_frequency = 0.0f * TWOPI;
-    float g_spatial_frequency = 0.01f * TWOPI;
-    float g_radius = 200.0f;
-    float g_contrast = 1.0f;
-
-    if (narg > 1)
-    {
-        g_orientation = atof(args[1]) * DEG2RAD;
-        if (narg > 2)
-        {
-            // cycles-per-second to radians-per-second
-            g_temporal_frequency = atof(args[2]) * TWOPI;
-        }
-        if (narg > 3)
-        {
-            // cycles-per-pixel to radians-per-pixel
-            g_spatial_frequency = atof(args[3]) * TWOPI;
-        }
-        if (narg > 4)
-        {
-            g_contrast = atof(args[4]);
-        }
-        if (narg > 5)
-        {
-            g_radius = atof(args[5]);
-        }
-    }
-
-    printf("O: %f | T: %f | S: %f | C: %f | R: %f\n",
-        g_orientation, g_temporal_frequency, g_spatial_frequency, g_contrast, g_radius);
 
 	glfwMakeContextCurrent(window);
 
@@ -91,11 +81,6 @@ int main(int narg, char** args)
 
     // initialize VBO
     GLVBO vbo;
-
-	// Create and compile our GLSL program from the shaders
-	// GLuint programID = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
-
-    /* ---------------------------------------------------------------------- */
 
 #ifdef USE_OGL_33
     // Create and compile our GLSL program from the shaders
@@ -112,143 +97,12 @@ int main(int narg, char** args)
         return -1;
     }
 
-    /* ---------------------------------------------------------------------- */
+    // RUN TRIALS
+    run(program, rs, window);
 
-    // Get a handle for our uniform program variables
-    GLint center = program.getUniform("center");
-    GLint radius = program.getUniform("radius");
-    GLint freq = program.getUniform("freq");
-    GLint contrast = program.getUniform("contrast");
-    GLint ori = program.getUniform("ori");
-
-    GLint phase = program.getAttribute("phase");
-
-    GLint vertex = program.getAttribute("vertexPosition");
-    /* ---------------------------------------------------------------------- */
-
-    static const GLfloat g_vertex_buffer_data[] = {
-		-1.0f, -1.0f, 0.0f,
-		 1.0f, -1.0f, 0.0f,
-         1.0f,  1.0f, 0.0f,
-		-1.0f,  1.0f, 0.0f,
-	};
-
-	GLuint vertexbuffer;
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
-
-    static const float position[2] = {(float)width / 2.0f, (float)height / 2.0f};
-
-    // rad-per-frame = rad-per-sec * sec-per-frame
-    float rad_per_frame = g_temporal_frequency * refresh_interval;
-    printf("Rad-per-frame: %f [%f]\n", rad_per_frame, refresh_interval);
-
-    unsigned int sleep_duration = (unsigned int)(refresh_interval * 1000000.0f) - 6000;
-    printf("Sleep duration: %d\n", (int)sleep_duration);
-
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
-    sleep(2);
-
-    float cur_phase = TWOPI;
-    float tstart = glfwGetTime();
-    float tnow, tlast, t1, t2;
-
-    t1 = tnow = tstart;
-
-    int inc = 0;
-
-	do
-    {
-		// Clear the screen
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		// Use our shader
-        program.use();
-
-        glUniform2fv(center, 1 , position);
-        glUniform1f(radius, g_radius);
-        glUniform1f(freq, g_spatial_frequency);
-        glUniform1f(contrast, g_contrast);
-        glUniform1f(ori, g_orientation);
-
-        glVertexAttrib1f(phase, cur_phase);
-
-		// 1rst attribute buffer : vertices
-#ifdef USE_OGL_33
-		glEnableVertexAttribArray(vertex);
-#else
-        glEnableVertexAttribArray(0);
-#endif
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glVertexAttribPointer(
-#ifdef USE_OGL_33
-			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-#else
-            vertex,
-#endif
-			3,                  // size
-			GL_FLOAT,           // type
-			GL_FALSE,           // normalized?
-			0,                  // stride
-			(void*)0            // array buffer offset
-		);
-
-		// draw window size rect
-        glDrawArrays(GL_TRIANGLE_FAN, 0, 4); // 4 indices starting at 0 -> 1 rectangle
-
-		glDisableVertexAttribArray(vertex);
-
-		// Swap buffers
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-
-        tlast = tnow;
-        tnow = glfwGetTime();
-
-        // make 0deg = right, 90deg = up, etc...
-        cur_phase -= (tnow - tlast) * g_temporal_frequency;
-
-        if (cur_phase <= 0.0f)
-        {
-            cur_phase = TWOPI;
-        }
-
-        if (tnow - tstart > 5.0f)
-        {
-            break;
-        }
-
-        usleep(sleep_duration);
-
-        if (inc % 10 == 0)
-        {
-            float t2 = glfwGetTime();
-            printf("%d: %f\n", inc, (t2 - t1) / 10);
-            t1 = t2;
-        }
-
-        ++inc;
-
-	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0 );
-
-   printf("[INFO]: left main render loop @ %f\n", glfwGetTime());
-
-   // glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
-   glClear( GL_COLOR_BUFFER_BIT );
-   glfwSwapBuffers(window);
-
-   printf("[INFO]: screen cleared @ %f\n", glfwGetTime());
-   sleep(1);
+    printf("[INFO]: left main render loop @ %f\n", glfwGetTime());
 
 	// Cleanup VBO
-	glDeleteBuffers(1, &vertexbuffer);
     vbo.close();
     program.close();
     printf("[INFO]: deleted buffers @ %f\n", glfwGetTime());
